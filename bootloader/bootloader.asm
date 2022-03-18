13b3dc4833067dedd1c6bdf8081fbc2a044e5315
drop-stones <shizukuishi.s.aa@m.titech.ac.jp>
2022-03-18 23:04:22 +0900

    Load kernel code in C

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -4,9 +4,6 @@
 ;*************************************************
 
 [bits 16]     ; tell NASM this is 16 bit code
-[org 0x7c00]  ; tell NASM to start outputting stuff at offset 0x7c00
-              ; All labels are added 0x7c00 for simplicity
-              ; BIOS always load boot sector to the address 0x7c00
 
 start:
   jmp boot
@@ -14,24 +11,13 @@ start:
 boot:
   mov [BOOT_DRIVE], dl  ; BIOS stores our boot drive in DL
 
-  mov bp, 0x9000  ; Here we set our stack safely
+  mov bp, 0x9000  ; Set-up stack
   mov sp, bp
 
   mov si, MSG_REAL_MODE
   call Print
 
-  mov ax, 0x50    ; ES:BX = pointer to buffer
-  mov es, ax      ; ES = 0x50
-  xor bx, bx      ; bx = 0x00
-
-  mov dh, 2             ; read 5 sectors
-  mov dl, [BOOT_DRIVE]  ; drive number
-  call LoadDisk
-
-  mov al, [0x500]       ; al = 0xda
-  call PutHexChar       ; print al as hex
-  mov al, [0x500 + 512] ; al = 0xce
-  call PutHexChar       ; print al as hex
+  call LoadKernel ; Load our kernel
 
   call SwitchIntoPM
 
@@ -42,16 +28,37 @@ boot:
 %include "gdt.asm"
 %include "switch_into_pm.asm"
 
+[bits 16]
+LoadKernel:
+  mov si, MSG_LOAD_KERNEL
+  call Print
+
+  cli                       ; clear all interrupt
+  cld                       ; 
+
+  mov ax, 0x50               ; Set-up parameters
+  mov es, ax                ; ES:BX = pointer to buffer
+  mov bx, 0x0
+  mov dh, 15                ; DH = number of sectors to read
+  mov dl, [BOOT_DRIVE]      ; DL = drive number
+  call LoadDisk
+
+  ret
+
 [bits 32]
 BEGIN_PM:
   mov esi, MSG_PROT_MODE
   call PrintPM
+
+  jmp [KERNEL_OFFSET + 0x18]
   hlt
 
 ; Global variables
+KERNEL_OFFSET equ 0x500  ; This is the memory offset to which we will load kernel
 BOOT_DRIVE db 0
 MSG_REAL_MODE db "Welcome to My Operating System in 16-bit Real Mode!", 0xa, 0xd, 0x0
 MSG_PROT_MODE db "Successfully landed in 32-bit Protected Mode!", 0xa 0xd, 0x0
+MSG_LOAD_KERNEL db "Loading kernel into memory ...", 0xa, 0xd, 0x0
 
 times 510 - ($-$$) db 0   ; We have to be 512 bytes. Clear the rest of the bytes with 0
                           ; '$' evaluates to the current assembly position
